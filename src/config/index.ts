364872ff85d87e0e31a0dccc664191672e56ee9e
YumaEndo <ey0104.work@gmail.com>
2024-08-27 23:03:33 +0900

    Next.js 環境構築

--- src/config/index.ts
+++
@@ -1,2 +0,0 @@
-export const GITHUB_USER_NAME = "y-endo-dev";
-export const GITHUB_API_URL = `https://api.github.com/users/${GITHUB_USER_NAME}`;
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export interface AppBarCategories {
-  id: string;
-  title: string;
-}
